074cae7195b7213f7c6444aeaa7b0695bcc1985d
keisku <keisuke.umegaki.630@gmail.com>
2022-03-09 08:37:48 +0900

    fix proxy logger handler

--- additional_endpoints/main.go
+++ additional_endpoints/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"io"
 	"net/http"
 	"os"
 	"os/signal"
@@ -38,13 +37,11 @@ type Handler struct {
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.Info("receive Datadog Agent request", zap.String("request_url", r.URL.String()))
+	h.Info(
+		"receive Datadog Agent request",
+		zap.String("request_url", r.URL.String()),
+		zap.Any("request_headers", r.Header),
+	)
 	defer r.Body.Close()
-	b, err := io.ReadAll(r.Body)
-	if err != nil {
-		h.Error("read request body", zap.Error(err))
-		return
-	}
-	h.Info("read request body", zap.String("body", string(b)))
-	w.WriteHeader(http.StatusBadGateway)
+	w.WriteHeader(http.StatusOK)
 }
