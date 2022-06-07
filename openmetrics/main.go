e01335f2919fc931230bcee4c1a181eb3916edfe
keisku <keisuke.umegaki.630@gmail.com>
2022-06-07 15:33:34 +0900

    add openmetrics

---
+++ openmetrics/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "golang",
+			Name:      "my_counter",
+			Help:      "This is my counter",
+		})
+
+	gauge = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "golang",
+			Name:      "my_gauge",
+			Help:      "This is my gauge",
+		})
+
+	histogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "golang",
+			Name:      "my_histogram",
+			Help:      "This is my histogram",
+		})
+
+	summary = prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Namespace: "golang",
+			Name:      "my_summary",
+			Help:      "This is my summary",
+		})
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	rand.Seed(time.Now().Unix())
+
+	http.Handle("/metrics", promhttp.Handler())
+	prometheus.MustRegister(counter)
+	prometheus.MustRegister(gauge)
+	prometheus.MustRegister(histogram)
+	prometheus.MustRegister(summary)
+	s := &http.Server{Addr: ":8080", Handler: http.DefaultServeMux}
+	go func() {
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			_ = s.Shutdown(context.Background())
+			log.Println("Received signal, exiting")
+			return
+		default:
+			counter.Add(rand.Float64() * 5)
+			gauge.Add(rand.Float64()*15 - 5)
+			histogram.Observe(rand.Float64() * 10)
+			summary.Observe(rand.Float64() * 10)
+			time.Sleep(time.Second)
+		}
+	}
+}
